e72cf32dd2fb60ba7083d8ced2ae51b507ed8f5f
Md-Jarif-Hosain <hosainjarif@gmail.com>
2023-04-12 16:34:58 +0600

    fix bugs

--- Github-profile-api/assets/js/main.js
+++ Github-profile-api/assets/js/main.js
@@ -1,16 +1,57 @@
 /** @format */
+/**
+ * Author : Md Jarif Hosain.
+ * Date : April 12, 2023.
+ *
+ * Basic Github API project.
+ */
 
-const apiUrl = "https://api.github.com/users/";
+// Globals
+const APIURL = "https://api.github.com/users/";
 const main = document.getElementById("main");
 const searchInput = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
 const form = document.getElementById("form");
 
-async function getUser(user) {
-  const resp = await fetch(apiUrl + user);
+// event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  searchUserHandler();
+});
+searchBtn.addEventListener("click", function (e) {
+  searchUserHandler();
+});
+function searchUserHandler() {
+  const user = searchInput.value;
+  if (user) {
+    getUser(user);
+    searchInput.value = " ";
+  }
+  window.reload();
+}
+
+// Async functions
+async function getUser(username) {
+  const resp = await fetch(APIURL + username);
   const resData = await resp.json();
   createUrCard(resData);
+  getRepos(username);
 }
+
+async function getRepos(username) {
+  const resp = await fetch(APIURL + username + "/repos");
+  const respData = await resp.json();
+
+  addReposToCard(respData);
+}
+
+// DOM Functions
+
+/**
+ * Creating a user card with api's infos
+ * @param {object} user
+ * @returns {DOM Element}
+ */
 function createUrCard(user) {
   const cardHTML = `
   <div class="card">
@@ -19,33 +60,53 @@ function createUrCard(user) {
       </div>
       <div class = "user-info">
         <h2>${user.name}</h2>
-        <p>${user.bio}</p>
+        <p>${user.location}</p>
+        <p class = "bio">${user.bio}</p>
+
 
         <ul class="info">
-            <li>${user.followers}</li>
-            <li>${user.following}</li>
-            <li>${user.public_repos}</li>
+            <li>${user.followers}<strong>Followers.</strong></li>
+            <li>${user.following}<strong>Following.</strong></li>
+            <li>${user.public_repos}<strong>Repos.</strong></li>
         </ul>
+
+        <div class="repos" id="repos"></div>
       </div>
-    </div>
+
+  </div>
 
     `;
   main.innerHTML = cardHTML;
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  searchUserHandler();
-});
-searchBtn.addEventListener("click", function (e) {
-e.preventDefault();
-searchUserHandler();
-});
-function searchUserHandler() {
-  const user = searchInput.value;
+/**
+ * Get users public-repos. and showing first 10 repos
+ * @param {Array of object} repos
+ * @returns {DOM Element}
+ */
+function addReposToCard(repos) {
+  const reposEl = document.getElementById("repos");
+  if (repos.length > 10) {
+    repos.slice(0, 10).forEach((repo) => {
+      const repoEl = document.createElement("a");
+      repoEl.classList.add("repo");
 
-  if (user) {
-    getUser(user);
-    searchInput.value = " ";
+      repoEl.href = repo.html_url;
+      repoEl.target = "_blank";
+      repoEl.innerText = repo.name;
+
+      reposEl.appendChild(repoEl);
+    });
+  } else {
+    repos.forEach((repo) => {
+      const repoEl = document.createElement("a");
+      repoEl.classList.add("repo");
+
+      repoEl.href = repo.html_url;
+      repoEl.target = "_blank";
+      repoEl.innerText = repo.name;
+
+      reposEl.appendChild(repoEl);
+    });
   }
 }
